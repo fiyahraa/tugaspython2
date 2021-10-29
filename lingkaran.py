c8f4d87ff96223a3cfb9fb8e950b3e60039f7fd4
fiyahraa <luthfiyahza19@gmail.com>
2021-10-29 20:03:30 +0700

    menambahkan file program

---
+++ lingkaran.py
@@ -0,0 +1,20 @@
+#Rumus Lingkaran
+
+print("### Tugas Pemrograman ###")
+print("nama: Luthfiyah")
+print("NIM : 312110131")
+print("Tema : Menghitung luas dan keliling lingkaran")
+
+print("###############")
+print("###############")
+print("###############")
+
+print("Program Menghitung Luas dan Keliling Lingkaran")
+r = int(input("Masukkan jari - jari = "))
+
+phi = 22/7
+luas = phi*r*r
+kel = 2*phi*r
+
+print("Luas Lingkaran adalah        =",luas)
+print("Keliling Lingkarang adalah       =",kel)
